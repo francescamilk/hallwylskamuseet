2738bf8bdae5ed31f480704eeb23294a48e2b132
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-07 15:22:41 +0100

    empty scene init

--- src/main.js
+++ src/main.js
@@ -1,2 +1,57 @@
 import * as THREE from 'three';
 import GUI from 'lil-gui';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
+
+// Debugger init
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector('#webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Window
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// handle resize callback
+window.addEventListener('resize', () => {
+    // update sizes
+    sizes.width  = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update camera
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    // update renderer
+    render.setSize(sizes.width, sizes.height);
+    render.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    45,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+const animLoop = () => {
+    renderer.render(scene, camera);
+}
+
+animLoop();
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true,
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+}
