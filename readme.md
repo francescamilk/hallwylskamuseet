ac518722fdb9eee060a23d881a26a6b3675be696
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-07 16:29:36 +0100

    load model & setup camera

--- readme.md
+++ readme.md
@@ -14,3 +14,12 @@ npm run dev
 # Build for production in the dist/ directory
 npm run build
 ```
+
+## Credits
+
+Model Information:
+* title:	The Great Drawing Room
+* source:	https://sketchfab.com/3d-models/the-great-drawing-room-feb9ad17e042418c8e759b81e3b2e5d7
+* author:	The Hallwyl Museum (Hallwylska museet) (https://sketchfab.com/TheHallwylMuseum)
+
+This work is based on "The Great Drawing Room" (https://sketchfab.com/3d-models/the-great-drawing-room-feb9ad17e042418c8e759b81e3b2e5d7) by The Hallwyl Museum (Hallwylska museet) (https://sketchfab.com/TheHallwylMuseum) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import GUI from 'lil-gui';
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
-// Debugger init
+// Gui init
 const gui = new GUI();
 
 // Canvas
@@ -28,30 +29,73 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 
     // update renderer
-    render.setSize(sizes.width, sizes.height);
-    render.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 });
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-    45,
+    60,
     sizes.width / sizes.height,
     0.1,
     100
 );
 
+camera.position.set(-4.37, 2.52, 3.51);
 scene.add(camera);
+
+// &plug gui
+gui
+    .add(camera, 'fov')
+    .min(10).max(150).step(0.1)
+    .name('Camera field of view')
+    .onChange(() => {
+        camera.updateProjectionMatrix();
+    });
+
+gui
+    .add(camera.position, 'x')
+    .min(-10).max(10).step(0.01)
+    .name('Camera position x');
+
+gui
+    .add(camera.position, 'y')
+    .min(-10).max(10).step(0.01)
+    .name('Camera position y');
+
+gui
+    .add(camera.position, 'z')
+    .min(-10).max(10).step(0.01)
+    .name('Camera position z');
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ 
+    canvas,
+    precision: 'mediump'  
+});
+
 renderer.setSize(sizes.width, sizes.height);
+// vivid colors
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+// Model load
+const loader = new GLTFLoader();
+
+loader.load('/model/swedish-royal/scene.gltf', (gltf) => {
+    const model = gltf.scene;
+    scene.add(model);
+});
 
 // Animate
 const animLoop = () => {
+    controls.update();
     renderer.render(scene, camera);
 }
 
+// built-in vr loop
+renderer.setAnimationLoop(animLoop);
 animLoop();
